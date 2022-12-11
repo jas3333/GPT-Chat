e92e71ffe6f809740d3192256048a612d59a6480
jas3333 <accounts1@protonmail.com>
2022-12-11 14:02:41 -0800

    Working on textarea

--- client/src/components/Prompt.js
+++ client/src/components/Prompt.js
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+
 const Prompt = ({ question, setQuestion, onSubmit, loading }) => {
+    const [rows, setRows] = useState(1);
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && event.shiftKey && rows !== 5) {
+            setRows(rows + 1);
+        } else if (event.key === 'Del') {
+            setRows(rows - 1);
+        } else if (event.keyCode === 13) {
+            event.preventDefault();
+            onSubmit();
+        }
+    };
     return (
         <div className='container auto content-center align mg-bot-lg '>
-            <form className='size-form-special ' onSubmit={onSubmit}>
-                <input
-                    type='text'
-                    name=''
+            <form onSubmit={onSubmit}>
+                <textarea
+                    rows={rows}
+                    resize='none'
                     value={question}
-                    className='input-text-lg full bg-mid'
-                    placeholder='Ask a question'
                     onChange={(event) => setQuestion(event.target.value)}
+                    cols='50'
+                    className='input-text'
+                    onKeyDown={handleKeyDown}
                     autoFocus={true}
                 />
             </form>
