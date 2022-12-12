eef03fab014821b99f3fb6d0153702eee5bdad05
jas3333 <accounts1@protonmail.com>
2022-12-11 16:28:29 -0800

    Updated chatbox

--- client/src/components/Prompt.js
+++ client/src/components/Prompt.js
@@ -6,28 +6,33 @@ const Prompt = ({ question, setQuestion, onSubmit, loading }) => {
     const handleKeyDown = (event) => {
         if (event.key === 'Enter' && event.shiftKey && rows !== 5) {
             setRows(rows + 1);
-        } else if (event.key === 'Del') {
-            setRows(rows - 1);
-        } else if (event.keyCode === 13) {
+        }
+    };
+    const handleSubmit = (event) => {
+        if (event.keyCode === 13 && !event.shiftKey) {
             event.preventDefault();
-            onSubmit();
+            setRows(1);
+            onSubmit(event);
         }
     };
     return (
         <div className='container auto content-center align mg-bot-lg '>
-            <form onSubmit={onSubmit}>
-                <textarea
-                    rows={rows}
-                    resize='none'
-                    value={question}
-                    onChange={(event) => setQuestion(event.target.value)}
-                    cols='50'
-                    className='input-text'
-                    onKeyDown={handleKeyDown}
-                    autoFocus={true}
-                />
-            </form>
-            <div className='loader '>{loading && <div className='spinner'></div>}</div>
+            <div className='container'>
+                <form onKeyDown={handleKeyDown} className=''>
+                    <textarea
+                        rows={rows}
+                        resize='none'
+                        value={question}
+                        onChange={(event) => setQuestion(event.target.value)}
+                        cols='50'
+                        className='input-text'
+                        onKeyDown={handleSubmit}
+                        autoFocus={true}
+                        placeholder='Ask a question'
+                    />
+                </form>
+                <div className='loader '>{loading && <div className='spinner '></div>}</div>
+            </div>
         </div>
     );
 };
